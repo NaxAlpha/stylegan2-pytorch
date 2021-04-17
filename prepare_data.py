98d621b47f028e83512d01bb50f8877a889527d7
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2021-04-17 11:16:36 +0500

    add option for dynamic sized data loading

--- prepare_data.py
+++ prepare_data.py
@@ -1,61 +1,41 @@
+import os
+import shutil
 import argparse
 from io import BytesIO
 import multiprocessing
-from functools import partial
 
-from PIL import Image
 import lmdb
 from tqdm import tqdm
+from PIL import Image
 from torchvision import datasets
-from torchvision.transforms import functional as trans_fn
 
 
-def resize_and_convert(img, size, resample, quality=100):
-    img = trans_fn.resize(img, size, resample)
-    img = trans_fn.center_crop(img, size)
+def convert(img, quality=100):
     buffer = BytesIO()
     img.save(buffer, format="jpeg", quality=quality)
     val = buffer.getvalue()
-
     return val
 
 
-def resize_multiple(
-    img, sizes=(128, 256, 512, 1024), resample=Image.LANCZOS, quality=100
-):
-    imgs = []
-
-    for size in sizes:
-        imgs.append(resize_and_convert(img, size, resample, quality))
-
-    return imgs
-
-
-def resize_worker(img_file, sizes, resample):
+def resize_worker(img_file, quality=100):
     i, file = img_file
     img = Image.open(file)
     img = img.convert("RGB")
-    out = resize_multiple(img, sizes=sizes, resample=resample)
-
+    out = convert(img, quality)
     return i, out
 
 
-def prepare(
-    env, dataset, n_worker, sizes=(128, 256, 512, 1024), resample=Image.LANCZOS
-):
-    resize_fn = partial(resize_worker, sizes=sizes, resample=resample)
-
-    files = sorted(dataset.imgs, key=lambda x: x[0])
-    files = [(i, file) for i, (file, label) in enumerate(files)]
+def prepare(env, imgs, n_worker):
+    files = sorted(imgs)
+    files = [(i, file) for i, file in enumerate(files)]
     total = 0
 
     with multiprocessing.Pool(n_worker) as pool:
-        for i, imgs in tqdm(pool.imap_unordered(resize_fn, files)):
-            for size, img in zip(sizes, imgs):
-                key = f"{size}-{str(i).zfill(5)}".encode("utf-8")
+        for i, img in tqdm(pool.imap_unordered(resize_worker, files)):
+            key = f"{str(i).zfill(5)}".encode("utf-8")
 
-                with env.begin(write=True) as txn:
-                    txn.put(key, img)
+            with env.begin(write=True) as txn:
+                txn.put(key, img)
 
             total += 1
 
@@ -65,12 +45,10 @@ def prepare(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Preprocess images for model training")
-    parser.add_argument("--out", type=str, help="filename of the result lmdb dataset")
     parser.add_argument(
-        "--size",
+        "--out",
         type=str,
-        default="128,256,512,1024",
-        help="resolutions of images for the dataset",
+        help="filename of the result lmdb dataset",
     )
     parser.add_argument(
         "--n_worker",
@@ -79,23 +57,26 @@ if __name__ == "__main__":
         help="number of workers for preparing dataset",
     )
     parser.add_argument(
-        "--resample",
+        "path",
         type=str,
-        default="lanczos",
-        help="resampling methods for resizing images",
+        help="path to the image dataset",
     )
-    parser.add_argument("path", type=str, help="path to the image dataset")
 
     args = parser.parse_args()
 
-    resample_map = {"lanczos": Image.LANCZOS, "bilinear": Image.BILINEAR}
-    resample = resample_map[args.resample]
+    imgs = map(
+        lambda fn: os.path.join(args.path, fn),
+        filter(
+            lambda fn: any(
+                map(
+                    lambda ext: fn.endswith(ext),
+                    datasets.folder.IMG_EXTENSIONS,
+                )
+            ),
+            os.listdir(args.path),
+        ),
+    )
 
-    sizes = [int(s.strip()) for s in args.size.split(",")]
-
-    print(f"Make dataset of image sizes:", ", ".join(str(s) for s in sizes))
-
-    imgset = datasets.ImageFolder(args.path)
-
-    with lmdb.open(args.out, map_size=1024 ** 4, readahead=False) as env:
-        prepare(env, imgset, args.n_worker, sizes=sizes, resample=resample)
+    shutil.rmtree(args.out)
+    with lmdb.open(args.out, map_size=200 * 1024 ** 2, readahead=False) as env:
+        prepare(env, imgs, args.n_worker)
